60bcd4897c159496309e5cacb776ddef0c34087e
Benjamin Feldman <benjamin.feldman@datadoghq.com>
2024-12-02 14:50:32 +0100

    Add test: parse entire call stack

--- test/stacks2doc/stack_test.clj
+++ test/stacks2doc/stack_test.clj
@@ -7,6 +7,9 @@
 
 (def TEST_STACK_FRAME "main:11, ChatApp (com.example.chat)")
 
+(def TEST_CALL_STACK "$bang:182, RepointableActorRef (akka.actor)
+tell:131, ActorRef (akka.actor)")
+
 (deftest test-parse-method-name
   (testing (let [stack (stack-from-source TEST_STACK_FRAME)
                  stack-frame (first stack)]
@@ -26,3 +29,9 @@
   (testing (let [stack (stack-from-source TEST_STACK_FRAME)
                  stack-frame (first stack)]
              (is (= "com.example.chat" (:package stack-frame))))))
+
+(deftest test-parse-source
+  (testing (let [[first-frame second-frame] (stack-from-source TEST_CALL_STACK)]
+             (is (and (= {:method "$bang" :line-number 182 :classname "RepointableActorRef" :package "akka.actor"} first-frame)
+                  (= {:method "tell" :line-number 131 :classname "ActorRef" :package "akka.actor"} second-frame)))
+             )))
