84d1d183ff6c628ca72191c5901d6f5f7f0aaa3f
Jonathan Boccara <jonathan.boccara@datadoghq.com>
2024-12-02 16:43:58 +0100

    Remove dummy app

--- src/stacks2doc/app.cljs
+++ src/stacks2doc/app.cljs
@@ -1,22 +1,11 @@
 (ns stacks2doc.app
   (:require [reagent.core :as r]
-            [stacks2doc.dummy-calculator :as dummy-calculator]
             [stacks2doc.stack :refer [packages-graph]]
-            [stacks2doc.graph :refer [make-graph]]
             [stacks2doc.mermaid :refer [to-flowchart]]))
 
-(declare diagram-textarea mermaid-output operands-form operand-input operand-textarea sum-form)
+(declare mermaid-output stack-input)
 
-(declare real-app stack-input)
-
-(def app
-  (fn []
-    [:<>
-     [real-app]
-     [:hr]
-     [sum-form]]))
-
-(defn real-app []
+(defn app []
   (let [stack-source (r/atom "")]
     (fn []
     [:<>
@@ -31,48 +20,6 @@
                 :name  "diagram-input"
                 :value @stack
                 :on-change #(reset! stack (-> % .-target .-value))}]])
-
-(defn sum-form []
-  (let [operands (r/atom {:operand1 2, :operand2 3})]
-    (fn []
-      (let [value1 (js/parseInt (:operand1 @operands))
-            value2 (js/parseInt (:operand2 @operands))
-            result (dummy-calculator/my_sum value1 value2)]
-        [:div
-         [:p {:class "dummy-style"} (str "Below this there should be two textboxes to input numbers, and the sum displayed here: " result ". This text should be in red.")]
-         (operands-form operands)]))))
-
-(defn operands-form [operands]
-  [:form
-   (operand-input operands :operand1 "First number:")
-   (operand-input operands :operand2 "Second number:")])
-
-(defn operand-input [operands kw display]
-  [:div
-   [:label {:for (name kw)} display]
-   (operand-textarea operands kw)])
-
-(defn operand-textarea [operands kw]
-  [:textarea {:type "text"
-              :id (name kw)
-              :name (name kw)
-              :value (kw @operands)
-              :on-change #(swap! operands assoc kw (-> % .-target .-value))}])
-
-(defn mermaid-form []
-  (let [diagram (r/atom "graph LR;A-->B")]
-  (fn []
-    [:div
-     [:p "Below this there should be a text input for a mermaid diagram, that should be rendered below it:"]
-     (diagram-textarea diagram)
-     #_[mermaid-output @diagram "test-mermaid"]])))
-
-(defn diagram-textarea [diagram]
-  [:textarea {:type "text"
-              :id "diagram-input"
-              :name  "diagram-input"
-              :value @diagram
-              :on-change #(reset! diagram (-> % .-target .-value))}])
 
 (defn mermaid-output [diagram id]
   (let [promise (.render js/window.mermaid "mermaid-css-id", diagram)]
--- src/stacks2doc/dummy_calculator.cljc
+++
@@ -1,6 +0,0 @@
-(ns stacks2doc.dummy-calculator)
-
-(defn my_sum
-  "If this code runs without issue then a lot of the configuration is correctly set up"
-  [x y]
-  (+ x y))
