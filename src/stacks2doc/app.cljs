8d64b6d87075f41b5c7e251c464fbe2ab8669ba5
Quentin Gendre Robin <quentin.gendrerobin@datadoghq.com>
2024-12-03 14:55:12 +0100

    Add label toggle

--- src/stacks2doc/app.cljs
+++ src/stacks2doc/app.cljs
@@ -7,20 +7,26 @@
 (declare mermaid-output remove-nth stack-input)
 
 (def use-detailed-graph (r/atom true))
+(def use-label (r/atom true))
 
 (defn app []
   (let [stack-sources (r/atom [""])]
     (fn []
       [:div {:class "p-4 space-y-4"}
-       [:button {:class "bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                 :on-click #(swap! use-detailed-graph not)}
-        "Toggle Graph Type"]
+       [:div {:class "space-x-4"}
+        [:button {:class "bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                  :on-click #(swap! use-detailed-graph not)}
+         "Toggle Graph Type"]
+        [:button {:class "bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600"
+                  :on-click #(swap! use-label not)}
+         "Toggle Labels"]]
        [:div {:class "grid grid-cols-3 gap-4"}
         (map #(stack-input stack-sources %) (vec (range (count @stack-sources))))]
        (try
          (mermaid-output (to-flowchart
                           (classes-graph-from-sources @stack-sources)
-                          :detailed @use-detailed-graph) "graph")
+                          :detailed @use-detailed-graph
+                          :label @use-label) "graph")
          (catch :default _
            [:div {:class "text-red-500 font-bold"}
             "Error: Invalid stack trace format."]))])))
