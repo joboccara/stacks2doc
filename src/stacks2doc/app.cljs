90a6768d034e347f6d8825299ea760e33bd879ad
Quentin Gendre Robin <quentin.gendrerobin@datadoghq.com>
2024-12-03 19:45:15 +0100

    Add debug toggle

--- src/stacks2doc/app.cljs
+++ src/stacks2doc/app.cljs
@@ -4,10 +4,11 @@
    [stacks2doc.mermaid :refer [to-flowchart]]
    [stacks2doc.stack :refer [classes-graph-from-sources]]))
 
-(declare mermaid-output remove-nth stack-input)
+(declare mermaid-output raw-output remove-nth stack-input)
 
 (def use-detailed-graph (r/atom true))
 (def use-label (r/atom true))
+(def use-debugging (r/atom false))
 (def base-url (r/atom ""))
 (def file-extension (r/atom ""))
 
@@ -24,7 +25,10 @@
          "Toggle Graph Type"]
         [:button {:class "bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600"
                   :on-click #(swap! use-label not)}
-         "Toggle Labels"]]
+         "Toggle Labels"]
+        [:button {:class "bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600"
+                  :on-click #(swap! use-debugging not)}
+         "Toggle Debug"]]
        [:div {:class "space-y-2"}
         [:div {:class "flex items-center space-x-2"}
          [:label {:class "font-bold text-gray-700 w-32"} "Base URL"]
@@ -41,12 +45,13 @@
        [:div {:class "grid grid-cols-3 gap-4"}
         (map #(stack-input stack-sources %) (vec (range (count @stack-sources))))]
        (try
-         (mermaid-output (to-flowchart
-                          (classes-graph-from-sources @stack-sources
-                                                      (tee @base-url)
-                                                      @file-extension)
-                          :detailed @use-detailed-graph
-                          :label @use-label) "graph")
+         ((if @use-debugging raw-output mermaid-output)
+          (to-flowchart
+           (classes-graph-from-sources @stack-sources
+                                       (tee @base-url)
+                                       @file-extension)
+           :detailed @use-detailed-graph
+           :label @use-label) "graph")
          (catch :default _
            [:div {:class "text-red-500 font-bold"}
             "Error: Invalid stack trace format."]))])))
@@ -71,6 +76,14 @@
 
 (defn remove-nth [arr n]
   (vec (concat (subvec arr 0 n) (subvec arr (inc n)))))
+
+(defn raw-output [diagram id]
+  [:textarea {:class "p-4 font-mono text-sm border rounded"
+              :style {:width "100%"}
+              :type "text"
+              :id id
+              :name "mermaid-debug"
+              :value diagram}])
 
 (defn mermaid-output [diagram id]
   (let [promise (.render js/window.mermaid "mermaid-css-id", diagram)]
