b3992ecfb96647e31e13f47f80ccf7c872e1c05f
Benjamin Feldman <benjamin.feldman@datadoghq.com>
2024-12-03 13:58:51 +0100

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx,cljs,cljc}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
